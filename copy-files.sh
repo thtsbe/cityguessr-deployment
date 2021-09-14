fee06a18ddf2a2120119f12d95c0680654441d2f
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-14 11:49:10 +0200

    service docker

--- copy-files.sh
+++ copy-files.sh
@@ -6,5 +6,4 @@ echo 'Copying files to server...' $server_ip
 
 scp -i ~/.ssh/id_rsa -r ./caddy root@$server_ip:../home
 scp -i ~/.ssh/id_rsa -r ./docker root@$server_ip:../home
-scp -i ~/.ssh/id_rsa -r ./caddy root@$server_ip:../home
 
