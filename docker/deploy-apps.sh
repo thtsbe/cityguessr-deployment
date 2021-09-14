d0177151d522eb6d42e996b929254951473fe11d
thtsbe <thomas.tschachtli@zuehlke.com>
2021-09-14 13:41:28 +0200

    deploy-apps.sh

--- docker/deploy-apps.sh
+++ docker/deploy-apps.sh
@@ -1,7 +1,6 @@
-docker-compose rm --stop -f cityguessr-app
-docker rmi thtsbe/cityguessr-app
-docker-compose up --build -d cityguessr-app
+docker-compose stop
 
-docker-compose rm --stop -f cityguessr-service
+docker rmi thtsbe/cityguessr-app
 docker rmi thtsbe/cityguessr-service
-docker-compose up --build -d cityguessr-serivce
+
+docker-compose up --build -d
